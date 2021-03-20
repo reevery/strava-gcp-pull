2cba99ad025b34fa612b6e38778a653b6868f424
Matthew Reeve <matthew@reevery.net>
2021-03-20 11:12:33 +0100

    Fix fetch_all which was not correctly excluding already-retrieved activities.

--- fetch_all.py
+++ fetch_all.py
@@ -14,6 +14,7 @@ logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
+    logging.getLogger('stravalib').setLevel(logging.INFO)
     logging.getLogger(__name__).setLevel(logging.DEBUG)
 
     # Check if a fresh token is required.
@@ -24,14 +25,16 @@ if __name__ == '__main__':
     # Initialise Google Cloud Storage.
     storage_client = storage.Client()
     bucket = storage_client.bucket(os.getenv('STORAGE_BUCKET_NAME'))
-    bucket_names = [os.path.splitext(b.name)[0] for b in bucket.list_blobs()]
-    logger.debug(bucket_names)
+    blob_names = [os.path.splitext(b.name)[0] for b in bucket.list_blobs()]
+    logger.debug(blob_names)
 
     # Download a list of activities.
     activities = client.get_activities()
-    logger.info('There are %s activities', len(list(activities)))
+    missing_activities = [a for a in activities if str(a.id) not in blob_names]
+    logger.info('There are %s activities, of which %s are missing.',
+                len(list(activities)), len(missing_activities))
 
     # Download each activity, if its name (Id) is not in your Storage Bucket.
-    for activity in [a for a in activities if a.name not in bucket_names]:
+    for activity in missing_activities:
         get_activity({'data': base64.b64encode(
             json.dumps({'object_id': activity.id}).encode('utf-8'))})
